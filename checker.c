7f1c16712305831d3de0eb24d16c0f56e514df46
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-08 21:18:13 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
-#include<stdbool.h>
 #include <assert.h>
 
-int Temp_OTR(float );
-int State_OTR(float );
-int Charge_OTR(float );
+int Temp_Range(float );
+int State_Range(float );
+int Charge_Range(float );
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {    
-    Temp_OTR(temperature);
-    State_OTR(soc);
-    Charge_OTR(chargeRate);
+#define RANGE_OK 1
+#define RANGE_NOT_OK 0
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {  
+int TR,SR,CR;
+    TR = Temp_Range(temperature);
+    SR = State_Range(soc);
+    CR = Charge_Range(chargeRate);
+    if ( TR && SR && CR)
+    return RANGE_OK;
     
-  return 1;
+    //printf("%d %d %d \n",  Temp_Range(temperature) , State_Range(soc) , Charge_Range(chargeRate));
+  return RANGE_NOT_OK;
 }
-int Temp_OTR(float temperature)
+int Temp_Range(float temperature)
 {
-    if(temperature < 0 || temperature > 45) 
-        printf("Temperature out of range!\n");
-    return 0;   
+    if(temperature <= 0 || temperature >= 45) {
+        printf("Temperature out of range! for %f\n",temperature);
+    return RANGE_NOT_OK; 
+    }
+    return RANGE_OK;
 }
-int State_OTR(float soc)
+int State_Range(float soc)
 {
-    if(soc < 20 || soc > 80) 
-        printf("State of Charge out of range!\n");
-    return 0;
+    if(soc <= 20 || soc >= 80) {
+        printf("State of Charge out of range! for %f\n",soc);
+    return RANGE_NOT_OK;
+    }
+    return RANGE_OK;
 }
-int Charge_OTR(float chargeRate)
+int Charge_Range(float chargeRate)
 {
-    if (chargeRate > 0.8) 
-        printf("Charge Rate out of range!\n");
-    return 0;
+    if (chargeRate<=0 || chargeRate >= 0.8) {
+        printf("Charge Rate out of range! for %f\n",chargeRate);
+    return RANGE_NOT_OK;
+    }
+    return RANGE_OK;
 }
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
