e729956aea7894d505a33f29b1200f8195c34127
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-08 21:32:31 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -11,9 +11,9 @@ int Charge_Range(float );
 int batteryIsOk(float temperature, float soc, float chargeRate) {  
 int TR,SR,CR;
     TR = Temp_Range(temperature);
-    SR = State_Range(soc);
+    //SR = State_Range(soc);
     CR = Charge_Range(chargeRate);
-    if ( TR && SR && CR){
+    if ( TR  && CR){
     return RANGE_OK;
     }
   return RANGE_NOT_OK;
