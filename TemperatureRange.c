be5f1a1f15f8403698e7304cd57b1b0a040af050
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-18 00:23:47 +0530

    Create TemperatureRange.c

---
+++ TemperatureRange.c
@@ -0,0 +1,29 @@
+
+#include <stdbool.h>
+#include "BMS.h"
+
+bool IsTemperatureValid(float temperature)
+{
+    if(temperature <= 0 || temperature >= 45) {
+        printf("Temperature out of range! for %f\n",temperature);
+    return RANGE_NOT_OK; 
+    }
+    return RANGE_OK;
+    
+    
+}
+bool Temp_Range(float temperature)
+{
+  bool batteryTemperatureResult = false;
+  
+  batteryTemperatureResult = IsTemperatureValid(temperature);
+  
+  if(batteryTemperatureResult)
+  {
+
+      batteryTemperatureResult = CheckForEarlyWarning(BATTERYPARAMETER_TEMPERATURE, temperature);
+ 
+  }
+  
+  return batteryTemperatureResult;
+}
